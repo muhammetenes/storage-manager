5a70169fc5ffe086832d350cd359df8f93c5b094
muhammetenes <muhammet.enesak@gmail.com>
2020-04-07 15:14:33 +0300

    added new static files and remove old files

---
+++ static/js/main.js
@@ -0,0 +1,338 @@
+AOS.init({
+    duration: 800,
+    easing: 'slide',
+    once: false
+});
+
+jQuery(document).ready(function ($) {
+
+    "use strict";
+
+    var siteMenuClone = function () {
+
+        $('.js-clone-nav').each(function () {
+            var $this = $(this);
+            $this.clone().attr('class', 'site-nav-wrap').appendTo('.site-mobile-menu-body');
+        });
+
+        setTimeout(function () {
+
+            var counter = 0;
+            $('.site-mobile-menu .has-children').each(function () {
+                var $this = $(this);
+
+                $this.prepend('<span class="arrow-collapse collapsed">');
+
+                $this.find('.arrow-collapse').attr({
+                    'data-toggle': 'collapse',
+                    'data-target': '#collapseItem' + counter,
+                });
+
+                $this.find('> ul').attr({
+                    'class': 'collapse',
+                    'id': 'collapseItem' + counter,
+                });
+
+                counter++;
+
+            });
+
+        }, 1000);
+
+        $('body').on('click', '.arrow-collapse', function (e) {
+            var $this = $(this);
+            if ($this.closest('li').find('.collapse').hasClass('show')) {
+                $this.removeClass('active');
+            } else {
+                $this.addClass('active');
+            }
+            e.preventDefault();
+
+        });
+
+        $(window).resize(function () {
+            var $this = $(this),
+                w = $this.width();
+
+            if (w > 768) {
+                if ($('body').hasClass('offcanvas-menu')) {
+                    $('body').removeClass('offcanvas-menu');
+                }
+            }
+        });
+
+        $('body').on('click', '.js-menu-toggle', function (e) {
+            var $this = $(this);
+            e.preventDefault();
+
+            if ($('body').hasClass('offcanvas-menu')) {
+                $('body').removeClass('offcanvas-menu');
+                $this.removeClass('active');
+            } else {
+                $('body').addClass('offcanvas-menu');
+                $this.addClass('active');
+            }
+        });
+
+        // click outisde offcanvas
+        $(document).mouseup(function (e) {
+            var container = $(".site-mobile-menu");
+            if (!container.is(e.target) && container.has(e.target).length === 0) {
+                if ($('body').hasClass('offcanvas-menu')) {
+                    $('body').removeClass('offcanvas-menu');
+                }
+            }
+        });
+    };
+    siteMenuClone();
+
+
+    var sitePlusMinus = function () {
+        $('.js-btn-minus').on('click', function (e) {
+            e.preventDefault();
+            if ($(this).closest('.input-group').find('.form-control').val() != 0) {
+                $(this).closest('.input-group').find('.form-control').val(parseInt($(this).closest('.input-group').find('.form-control').val()) - 1);
+            } else {
+                $(this).closest('.input-group').find('.form-control').val(parseInt(0));
+            }
+        });
+        $('.js-btn-plus').on('click', function (e) {
+            e.preventDefault();
+            $(this).closest('.input-group').find('.form-control').val(parseInt($(this).closest('.input-group').find('.form-control').val()) + 1);
+        });
+    };
+    // sitePlusMinus();
+
+
+    var siteSliderRange = function () {
+        $("#slider-range").slider({
+            range: true,
+            min: 0,
+            max: 500,
+            values: [75, 300],
+            slide: function (event, ui) {
+                $("#amount").val("$" + ui.values[0] + " - $" + ui.values[1]);
+            }
+        });
+        $("#amount").val("$" + $("#slider-range").slider("values", 0) +
+            " - $" + $("#slider-range").slider("values", 1));
+    };
+    // siteSliderRange();
+
+
+    var siteMagnificPopup = function () {
+        $('.image-popup').magnificPopup({
+            type: 'image',
+            closeOnContentClick: true,
+            closeBtnInside: false,
+            fixedContentPos: true,
+            mainClass: 'mfp-no-margins mfp-with-zoom', // class to remove default margin from left and right side
+            gallery: {
+                enabled: true,
+                navigateByImgClick: true,
+                preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
+            },
+            image: {
+                verticalFit: true
+            },
+            zoom: {
+                enabled: true,
+                duration: 300 // don't foget to change the duration also in CSS
+            }
+        });
+
+        $('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
+            disableOn: 700,
+            type: 'iframe',
+            mainClass: 'mfp-fade',
+            removalDelay: 160,
+            preloader: false,
+
+            fixedContentPos: false
+        });
+    };
+    siteMagnificPopup();
+
+
+    var siteCarousel = function () {
+        if ($('.nonloop-block-13').length > 0) {
+            $('.nonloop-block-13').owlCarousel({
+                center: false,
+                items: 1,
+                loop: true,
+                stagePadding: 0,
+                margin: 0,
+                autoplay: true,
+                nav: true,
+                navText: ['<span class="icon-arrow_back">', '<span class="icon-arrow_forward">'],
+                responsive: {
+                    600: {
+                        margin: 0,
+                        nav: true,
+                        items: 2
+                    },
+                    1000: {
+                        margin: 0,
+                        stagePadding: 0,
+                        nav: true,
+                        items: 3
+                    },
+                    1200: {
+                        margin: 0,
+                        stagePadding: 0,
+                        nav: true,
+                        items: 4
+                    }
+                }
+            });
+        }
+
+        $('.slide-one-item').owlCarousel({
+            center: false,
+            items: 1,
+            loop: true,
+            stagePadding: 0,
+            margin: 0,
+            autoplay: true,
+            pauseOnHover: false,
+            nav: true,
+            navText: ['<span class="icon-keyboard_arrow_left">', '<span class="icon-keyboard_arrow_right">']
+        });
+    };
+    siteCarousel();
+
+    var siteStellar = function () {
+        $(window).stellar({
+            responsive: false,
+            parallaxBackgrounds: true,
+            parallaxElements: true,
+            horizontalScrolling: false,
+            hideDistantElements: false,
+            scrollProperty: 'scroll'
+        });
+    };
+    siteStellar();
+
+    var siteCountDown = function () {
+
+        $('#date-countdown').countdown('2020/10/10', function (event) {
+            var $this = $(this).html(event.strftime(''
+                + '<span class="countdown-block"><span class="label">%w</span> weeks </span>'
+                + '<span class="countdown-block"><span class="label">%d</span> days </span>'
+                + '<span class="countdown-block"><span class="label">%H</span> hr </span>'
+                + '<span class="countdown-block"><span class="label">%M</span> min </span>'
+                + '<span class="countdown-block"><span class="label">%S</span> sec</span>'));
+        });
+
+    };
+    siteCountDown();
+
+    var siteDatePicker = function () {
+
+        if ($('.datepicker').length > 0) {
+            $('.datepicker').datepicker();
+        }
+
+    };
+    siteDatePicker();
+
+    function deleteItemsRequest(keys) {
+		return $.ajax({
+			url: deleteItemUrl,
+			method: "post",
+			data: {
+				keys: keys
+			}
+		})
+	}
+
+    function deleteItem(key, options) {
+        var photo_item = $(options["$trigger"][0]).parent();
+		var itemKey = options["$trigger"][0].dataset.caption;
+        deleteItemsRequest(itemKey.split()).done(function (response) {
+            $("#modalBody").text(response.message);
+            $("#exampleModalCenter").modal("show");
+		    if (!response.error){
+                photo_item.remove();
+                object_count--
+            }
+            $(".object-count").text(object_count)
+        });
+	}
+
+	function deleteItems(key, options) {
+        var itemKeys = $(".photo-item-checkbox:checked");
+        var keys = [];
+        var photo_items = [];
+        $.each(itemKeys, function (index, elem) {
+            var photo_item = $(elem).parent().parent();
+            var key = photo_item.children("a")[0].dataset.caption;
+            keys.push(key);
+            photo_items.push(photo_item)
+        });
+        deleteItemsRequest(keys).done(function (response) {
+            $("#modalBody").text(response.message);
+            $("#exampleModalCenter").modal("show");
+            if (!response.error){
+                $.each(photo_items, function (index, elem) {
+                    elem.remove();
+                });
+                object_count -= photo_items.length
+            }
+            $(".object-count").text(object_count)
+        })
+    }
+    // album.html page context menu
+	$.contextMenu({
+		selector: '.album-item',
+		callback: function(key, options) {
+			var m = "clicked: " + key;
+			window.console && console.log(m) || alert(m);
+		},
+		items: {
+			"rename": {name: "Rename", icon: "edit"},
+			"move": {name: "Move", icon: "paste"},
+			"copy": {name: "Copy", icon: "copy"},
+			"delete": {name: "Delete", icon: "delete", callback: deleteItem},
+			"sep1": "---------",
+			"move_selected": {name: "Move selected items", icon: "paste", disabled: function () {
+					return $(document).find(".photo-item-checkbox:checked").length <= 1;
+				}},
+			"copy_selected": {name: "Copy selected items", icon: "copy", disabled: function () {
+					return $(document).find(".photo-item-checkbox:checked").length <= 1;
+				}},
+			"delete_selected": {name: "Delete selected items", icon:"delete", disabled: function () {
+					return $(document).find(".photo-item-checkbox:checked").length <= 1;
+				}, callback: deleteItems},
+		}
+	});
+
+    function deleteBucketsRequest(buckets) {
+        $.ajax({
+            url: ""
+        })
+    }
+
+    function deleteBuckets(key, options) {
+
+    }
+
+    // bucket.html page context menu
+	$.contextMenu({
+		selector: '.bucket-item',
+		callback: function(key, options) {
+			var m = "clicked: " + key;
+			window.console && console.log(m) || alert(m);
+		},
+		items: {
+			"rename": {name: "Rename bucket", icon: "edit"},
+			"delete": {name: "Delete bucket", icon: "delete"},
+			"sep1": "---------",
+			"duplicate": {name: "Duplicate bucket", icon: "copy"},
+			"delete_selected": {name: "Delete selected buckets", icon:"delete", disabled: function () {
+					return $(document).find(".photo-item-checkbox:checked").length <= 1;
+				}},
+		}
+	});
+
+});
