7d22842cdff22b69f044c3b6fd3dcbe08ce715c1
muhammetenes <muhammet.enesak@gmail.com>
2020-04-26 23:39:05 +0300

    minor update

--- router/router.go
+++ router/router.go
@@ -55,8 +55,8 @@ func New() *echo.Echo {
 	e.GET("/list_buckets", base_handlers.ListBuckets)
 	e.POST("/create_bucket", base_handlers.CreateBucket)
 	e.GET("/:bucket/list_objects", base_handlers.ListBaseObjects)
-	e.GET("/:bucket/list_objects/:key", base_handlers.ListFolderObjects)
 	e.GET("/:bucket/list_objects_with_key", base_handlers.ListObjectsWithKey)
+	e.GET("/:bucket/list_objects/:key", base_handlers.ListFolderObjects)
 	e.POST("/:bucket/upload_file", base_handlers.UploadFileToBucket)
 
 	e.POST("/delete_buckets", base_handlers.DeleteBuckets)
