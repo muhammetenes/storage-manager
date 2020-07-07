257d14cee578b537bec2c0eac346bc94c92d9ee5
muhammetenes <muhammet.enesak@gmail.com>
2020-07-07 23:52:52 +0300

    fix path

--- handlers/awss3/aws_handler_test.go
+++ handlers/awss3/aws_handler_test.go
@@ -13,7 +13,7 @@ func TestLogin(t *testing.T) {
 	req := httptest.NewRequest(http.MethodGet, "/", nil)
 	rec := httptest.NewRecorder()
 	c := e.NewContext(req, rec)
-	c.SetPath("/users/:email")
+	c.SetPath("/login")
 	c.SetParamNames("email")
 	c.SetParamValues("jon@labstack.com")
 
