746997caa2e5a3bc618e8e0b6f1c2c960a0e01f3
muhammetenes <muhammet.enesak@gmail.com>
2020-07-07 23:50:15 +0300

    added test for login

--- handlers/awss3/aws_handler_test.go
+++ handlers/awss3/aws_handler_test.go
@@ -1,1 +1,25 @@
 package awss3
+
+import (
+	"github.com/labstack/echo"
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestLogin(t *testing.T) {
+	e := echo.New()
+	req := httptest.NewRequest(http.MethodGet, "/", nil)
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+	c.SetPath("/users/:email")
+	c.SetParamNames("email")
+	c.SetParamValues("jon@labstack.com")
+
+	// Assertions
+	if assert.NoError(t, h.getUser(c)) {
+		assert.Equal(t, http.StatusOK, rec.Code)
+		assert.Equal(t, userJSON, rec.Body.String())
+	}
+}
