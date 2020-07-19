f531f53d1c96adde3ddc060f1a8c1b018e145637
muhammetenes <muhammet.enesak@gmail.com>
2020-07-19 22:48:31 +0300

    fix

--- handlers/awss3/aws_handler_test.go
+++ handlers/awss3/aws_handler_test.go
@@ -2,16 +2,10 @@ package awss3
 
 import (
 	"github.com/labstack/echo"
-	"net/http"
-	"net/http/httptest"
 	"testing"
 )
 
 func TestLogin(t *testing.T) {
-	e := echo.New()
-	req := httptest.NewRequest(http.MethodPost, "/login", nil)
-	rec := httptest.NewRecorder()
-	c := e.NewContext(req, rec)
-	c.SetPath("/login")
+	echo.New()
 
 }
